ae4ef4739eafc9b3b69d98f73527dc668279e4de
UnknownHimS <resorxvelarde@gmail.com>
2024-11-13 11:23:10 +0800

    Update seartofMafia.js

--- seartofMafia.js
+++ seartofMafia.js
@@ -2,6 +2,21 @@ const darkmode = document.getElementById("darkmode");
 const home = document.getElementById('home');
 const title = document.getElementById('title');
 const sentenceContainer = document.getElementById('sentenceContainer');
+const chapter1 = document.getElementById('chapter1');
+const pre1 = document.getElementById('pre1');
+
+const preone = `   In a world where everyone has their own fears
+
+and weaknesses, a man named Denzo has none. He is a
+
+perpectionist, even about his appearance, allowing
+
+no flaws. Denzo does not let any emotion get in his
+
+way, and he is a genius with an IQ surpassing Albert
+
+Einstein's, scoring at 240.`;
+
 
 darkmode.onclick=function(){
     if(darkmode.textContent === 'darkmode🌙'){
@@ -14,12 +29,26 @@ darkmode.onclick=function(){
        sentenceContainer.style.fontWeight='bold';
        darkmode.textContent='lightmode⚡';
        darkmode.style.color='white';
+       sentenceContainer.style.margin='150px 0px';
+       title.style.margin='35px 400px';
+       chapter1.style.margin='90px 100px';
+       pre1.textContent=`
+          In a world where everyone has their own fears  and weaknesses, a man named Denzo has none. He is 
+         
+ a perpectionist, even about his appearance, allowing  no flaws. Denzo does not let any emotion get in his
+    
+ way, and he is a genius with an IQ surpassing Albert Einstein's, scoring at 240.
+    
+    
+`
+       
+       
     }
     else if(darkmode.textContent === 'lightmode⚡'){
         darkmode.textContent='darkmode🌙';
         
         document.body.style='white';
-        document.body.style.backgroundImage = `url('zafiarom.png')`;
+        document.body.style.backgroundImage = `url('/zafiarom.png')`;
         document.body.style.backgroundRepeat = 'no-repeat';
         document.body.style.bodybackgroundSize = '50%';
         document.body.style.backgroundPosition= 'right 0px';
@@ -29,6 +58,12 @@ darkmode.onclick=function(){
         sentenceContainer.style.fontWeight='100';
         title.style.textShadow=" 3px 3px 4px black";
         darkmode.style.color='black';
+        sentenceContainer.style.margin='245px 0px';
+        title.style.margin='100px 100px';
+        chapter1.style.margin='200px 40px'
+        pre1.textContent=preone;
+        
+        
    
         
 
